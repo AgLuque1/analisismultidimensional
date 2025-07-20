e5d24ffff88fe7765a3f0a625abf3c77d86ed01c
Antonio Granados <antoniotic31@gmail.com>
2025-07-20 10:41:35 +0200

    Arreglado mongo

--- fastapi/init_db.py
+++ fastapi/init_db.py
@@ -3,7 +3,6 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import time
 
 def crear_bds(nombre_bd, user, password, host="127.0.0.1", port=5432):
-    for _ in range(10):
         try:
             conn = psycopg2.connect(
                 dbname="postgres",
@@ -12,28 +11,26 @@ def crear_bds(nombre_bd, user, password, host="127.0.0.1", port=5432):
                 host=host,
                 port=port
             )
-            break
         except Exception as e:
+            import traceback
+            traceback.print_exc()
+            raise Exception("No se pudo conectar a postgre")
             print("Esperando POSTGRESQL.. {e}")
-            time.sleep(2)
 
-    else:
-        raise Exception ("No se pudo conectar a postgresql")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()
 
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = conn.cursor()
+        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (nombre_bd,))
+        existe = cursor.fetchone()
 
-    cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (nombre_bd,))
-    existe = cursor.fetchone()
+        if not existe:
+            cursor.execute(f'CREATE DATABASE "{nombre_bd}"')
+            print(f"Base de datos '{nombre_bd}' creada.")
+        else:
+            print(f"La base de datos '{nombre_bd}' ya existe.")
 
-    if not existe:
-        cursor.execute(f'CREATE DATABASE "{nombre_bd}"')
-        print(f"Base de datos '{nombre_bd}' creada.")
-    else:
-        print(f"La base de datos '{nombre_bd}' ya existe.")
-
-    cursor.close()
-    conn.close()
+        cursor.close()
+        conn.close()
 
 crear_bds("Modelo_AM", user="postgres", password="aaaa")
 crear_bds("DataWarehouse_AM", user="postgres", password="aaaa")
