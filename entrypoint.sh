8aee24c051b504396b2f601da085f7b2b6951077
Antonio Granados <antoniotic31@gmail.com>
2025-07-21 18:22:59 +0200

    Arreglado postgre

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,21 @@ sleep 5
 mongoimport --db EjemploVentas --collection meta --file /data/mongo-init/meta.json --jsonArray
 mongoimport --db EjemploVentas --collection data --file /data/mongo-init/data.json --jsonArray
 
-'''
-# Aplicamos migraciones de Django
-python bigdatamed-main/manage.py makemigrations --noinput
-python bigdatamed-main/manage.py migrate --noinput
 
-echo "Migraciones hechas"
+# Aplicamos migraciones de Django
+#python bigdatamed-main/manage.py makemigrations --noinput
+#python bigdatamed-main/manage.py migrate --noinput
+
+#echo "Migraciones hechas"
 
 # Creamos superusuario si no existe
-python bigdatamed-main/manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpass")
-EOF
-'''
+#python bigdatamed-main/manage.py shell <<EOF
+#from django.contrib.auth import get_user_model
+#User = get_user_model()
+#if not User.objects.filter(username="admin").exists():
+#    User.objects.create_superuser("admin", "admin@example.com", "adminpass")
+#EOF
+#'''
 
 # Lanzamos bigdatamed Django en background
 python bigdatamed-main/manage.py runserver 0.0.0.0:8000 &
