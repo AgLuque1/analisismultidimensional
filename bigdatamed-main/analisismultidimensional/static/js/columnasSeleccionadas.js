1fe8ebb11911c6ece38b1e0c6591615640b5952b
AgLuque1 <79659314+AgLuque1@users.noreply.github.com>
2025-09-03 08:55:33 +0200

    Update columnasSeleccionadas.js

--- bigdatamed-main/analisismultidimensional/static/js/columnasSeleccionadas.js
+++ bigdatamed-main/analisismultidimensional/static/js/columnasSeleccionadas.js
@@ -1,5 +1,4 @@
 //Función para añadir las columnas seleccionadas de la experimentación a los select de las medidas y de la estructura del cubo
-
 $(document).ready(function() {
     //Inicializamos Select2 en los selects
     $('#columnas').select2();
@@ -17,12 +16,9 @@ $(document).ready(function() {
       $('#niveles').empty();
       
       //Iteramos sobre el array, añadiendo cada nueva columna
-
       colsSeleccionadas.forEach((columna) => {
 
         var metadatosColumna = metadatos.find(obj => obj.Code === columna);
-        //var metadatosColumna = metadatos[0].Code;
-
         $('#niveles').append('<option value="' + columna + '">' + columna + ' - ' + metadatosColumna.VarType  + '</option>');
 
         if(metadatosColumna.VarType === 'Numerical'){
@@ -31,7 +27,6 @@ $(document).ready(function() {
         
       })
 
-     
       $('#columnasSeleccionadas').trigger('change');
       $('#niveles').trigger('change');
     });
